677d3c00c29332f5caf6a78b8282035d7edeb8f9
Tyler <tylermtu@gmail.com>
2022-12-12 16:19:57 -0500

    normalize

--- Math.hpp
+++ Math.hpp
@@ -7,24 +7,30 @@ namespace math
     const double pi = 3.141592653589793238463;
 
     template<class T>
-    inline T rad2deg(T rad)
+    inline T rad2deg(const T& rad)
     { return rad * (180 / pi); }
 
     template<class T>
-    inline T deg2rad(T deg)
+    inline T deg2rad(const T& deg)
     { return deg * (pi / 180); }
 
     template<class T>
     const auto clamp = std::clamp<T>;
 
     template<class T>
-    inline T stepify(T value, T step)
+    inline T stepify(const T& value, const T& step)
     {
         // return ( floor(value / step) * step );
         return step * std::round(value / step);
     }
 
+    template<class T>
+    float normalize(const T& val, const T& min, const T& max)
+    {
+        return (val - min) / (max - min);
+    }
+
     template <class T>
-    int sign(T value)
+    int sign(const T& value)
     { return (value > 0) - (value < 0); }
 }
