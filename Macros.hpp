4258af14895447bc8e8c3e8d8a0c22ff1b339ec8
Tyler <tylermtu@gmail.com>
2022-12-16 01:01:24 -0500

    more helper macros

--- Macros.hpp
+++ Macros.hpp
@@ -2,6 +2,13 @@
 
 #include <iostream>
 
+
+#define OS_WINDOWS _WIN32
+#define OS_MAC     __APPLE__
+#define OS_LINUX   __linux__ && __FreeBSD__
+
+#pragma region Assertions
+
 #ifndef _DEBUG
     #define _DEBUG 0
 #endif
@@ -40,12 +47,16 @@ if( !(x) ) \
 #define ASSERTWARN(x, str) ((void)0);
 #endif
 
-inline void debugWriteln(const std::string& str) noexcept
-{
-    if constexpr (_DEBUG) { std::cout << str << "\n"; }
-}
+#pragma endregion
 
-inline void debugWrite(const std::string& str) noexcept
-{
-    if constexpr (_DEBUG) { std::cout << str; }
-}
+// Create a bitwise OR operator for enums used as flags
+#define FLAG_ENUM(enumName)                                                  \
+inline enumName operator|(enumName a, enumName b)                            \
+{ return static_cast<enumName>(static_cast<int>(a) | static_cast<int>(b)); } \
+inline enumName& operator|=(enumName & a, const enumName & b)                \
+{ return a = a | b; }                                                        \
+inline bool operator&(enumName a, enumName b)                                \
+{ return static_cast<int>(a) & static_cast<int>(b); }
+
+#define STRING_MACRO_NAME(X) #X
+#define STRING_MACRO_VALUE(X) STRING_MACRO_NAME(X)
