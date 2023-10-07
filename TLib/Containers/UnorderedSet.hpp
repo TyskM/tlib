d9c03c35c6f22d579bb865f4fb50a94f7bf14f49
Tyler <tylermtu@gmail.com>
2023-10-07 02:22:18 -0400

    unordered set alias

---
+++ TLib/Containers/UnorderedSet.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+#include <TLib/EASTL.hpp>
+#include <EASTL/unordered_set.h>
+
+template <typename Value,
+          typename Hash = eastl::hash<Value>,
+          typename Predicate = eastl::equal_to<Value>,
+          typename Allocator = MiAllocator,
+          bool bCacheHashCode = false>
+using UnorderedSet = eastl::unordered_set<Value, Hash, Predicate, Allocator, bCacheHashCode>;
